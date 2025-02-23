506c0dd57bed32d08a6d2e7e58c9aeefe7e7c36b
engineeramangupta <amu.aman19@gmail.com>
2025-02-23 12:32:39 +0530

    added some models

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
 print('Adding a new branch to play with git')
+
+def model1():
+    print('This is model 1')
+
+#  use model
+
+model1()
+
+def model2():
+    print('This is model 2')
+
+#  use model
+
+model2()
